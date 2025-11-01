bf25a1425ecb254eb9f13cedd3d13d0bfa87e585
ReenahMae <rcampilanan.reenahc@gmail.com>
2025-11-01 13:39:13 +0800

    Initial commit with frontend, backend, and project files

---
+++ backend/connectivity-backend/src/main/java/com/connectivity/connectivity_backend/ConnectivityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.connectivity.connectivity_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectivityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectivityBackendApplication.class, args);
+	}
+
+}
---
+++ backend/connectivity-backend/src/test/java/com/connectivity/connectivity_backend/ConnectivityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectivity.connectivity_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectivityBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
